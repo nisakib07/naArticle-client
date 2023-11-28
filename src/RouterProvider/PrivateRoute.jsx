b655c83f454419a5d6d507fee6663dc7925ed053
nisakib07 <websakib07@gmail.com>
2023-11-28 13:58:13 +0600

    Private Route Implemented

---
+++ src/RouterProvider/PrivateRoute.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Blocks } from "react-loader-spinner";
+import PropTypes from "prop-types";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <Blocks
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+      />
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};
+
+export default PrivateRoute;
